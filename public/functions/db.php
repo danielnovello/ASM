8baa9e6f2b9debbd5e0e2eda69e256242efa2440
Daniel Novello <djquazzi@gmail.com>
2019-05-09 14:48:18 +0200

    Update db.php

--- public/functions/db.php
+++ public/functions/db.php
@@ -4,7 +4,7 @@ define('DB_HOST', 'localhost');
 define('DB_NAME', 'asm');
 define('DB_CHARSET', 'utf8');
 define('DB_USER', 'root');
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', '');
 $pdo = new PDO(
   "mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=".DB_CHARSET, 
   DB_USER, DB_PASSWORD, [
