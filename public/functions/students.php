6bdf26028f8a28fbdef02202f485778fb548acc7
djquazzi <djquazzi@gmail.com>
2019-10-17 12:26:51 +0200

    Fix DB script ordering

--- public/functions/students.php
+++ public/functions/students.php
@@ -13,7 +13,7 @@ header('Content-Type: text/csv');
 header('Content-Disposition: attachment; filename="students.csv"');
 $stmt = $pdo->prepare("SELECT * FROM `students`");
 $stmt->execute();
-echo '"person_id"' , "," , '"person_number"' , "," , '"first_name"' , "," , '"middle_name"' , "," , '"grade_level"' , "," , '"last_name"' , "," , '"email_address"' , "," , '"sis_username"' , "," , '"password_policy"', "," , '"location_id"' . PHP_EOL;
+echo '"person_id"' , "," , '"person_number"' , "," , '"first_name"' , "," , '"middle_name"' , "," , '"last_name"' , "," , '"grade_level"' , "," , '"email_address"' , "," , '"sis_username"' , "," , '"password_policy"', "," , '"location_id"' . PHP_EOL;
 while ($row = $stmt->fetch(PDO::FETCH_NAMED)) {
     echo '"' . $row['students_person_id'] . '"' . "," . '"' . $row['students_person_number'] . '"' . "," . '"' . $row['students_first_name'] . '"' . "," . '"' . $row['students_middle_name'] . '"' . "," . '"' . $row['students_last_name'] . '"' . "," . '"' . $row['students_grade_level'] . '"' . "," . '"' . $row['students_email_address'] . '"' . "," . '"' . $row['students_sis_username'] . '"' . "," . '"' . $row['students_password_policy'] . '"' . "," . '"' . $row['location_id'] . '"' . PHP_EOL;
 }
